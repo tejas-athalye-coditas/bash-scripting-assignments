ce0f03db079fcb3b1f11a12fd8eab9b019123f80
unknown <tejas.athalye@coditas.com>
2025-07-25 16:17:29 +0530

    Task 1 done

---
+++ assignment_2/string_input_ops.sh
@@ -0,0 +1,11 @@
+# Task 1
+
+#/bash/bin
+
+echo "Enter a string: "
+read string
+
+echo "The length of $string is ${#string}"
+reverse=$(echo $string | rev)
+echo "The reverse of $string is $reverse"
+echo ""
